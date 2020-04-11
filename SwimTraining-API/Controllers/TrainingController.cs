c977b91d567b41f8145ef7a985567d4e7f3d79ac
CristianQS <cristian_31teror@hotmail.com>
2020-04-11 14:35:13 +0100

    ignore

--- SwimTraining-API/Controllers/TrainingController.cs
+++ SwimTraining-API/Controllers/TrainingController.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using SwimTraining.Application.PrimaryAdapters;
+using SwimTraining.Domain;
 
 namespace SwimTraining_API.Controllers {
-    [Route("training")]
+    [Route("api/training")]
 
     public class TrainingController : Controller {
         // GET
         [HttpGet]
-        public ActionResult GetTrainingByUser(string userId) {
+        public List<Training> GetTrainingByUser(string userId) {
             var trainingList = new TrainingServices().GetTrainingBy(userId);
             return trainingList;
         }
--- SwimTraining.Application.PrimaryAdapters/TrainingServices.cs
+++ SwimTraining.Application.PrimaryAdapters/TrainingServices.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using SwimTraining.Domain;
 
 namespace SwimTraining.Application.PrimaryAdapters {
     public class TrainingServices {
+
         public TrainingServices() {
         }
 
         public List<Training> GetTrainingBy(string userId) {
-            return null;
+            Trainings.Add(training);
+
+            return Trainings;
         }
     }
 }
