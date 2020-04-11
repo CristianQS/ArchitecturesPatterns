410cd4ddaf477b3d307fdeeb94d92bd57e1cb2d8
CristianQS <cristian_31teror@hotmail.com>
2020-04-11 14:34:40 +0100

    adapter Postresql

---
+++ SwimTraining.Application.SecondaryPorts/TrainingRepositoryPort.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using SwimTraining.Domain;
+
+namespace SwimTraining.Application.SecondaryPorts {
+    public interface TrainingRepositoryPort {
+        List<Training> GetTrainingByUser();
+    }
+}
---
+++ SwimTraining.Infraestructure.SecondaryAdapters/TrainingRepositoryPostgreSQLAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using SwimTraining.Application.SecondaryPorts;
+using SwimTraining.Domain;
+
+namespace SwimTraining.Infraestructure.SecondaryAdapters {
+    public class TrainingRepositoryPostgreSQLAdapter : TrainingRepositoryPort {
+        public Training Training1 = new Training("Training 1", "ADescription", new DateTime(2020, 1, 1), null, "1234");
+        public Training Training2 = new Training("Training 2", "AnotherDescription", new DateTime(2020, 2, 2), null, "1234");
+        public List<Training> Trainings = new List<Training>();
+        public List<Training> GetTrainingByUser() {
+            Trainings.Add(Training1);
+            Trainings.Add(Training2);
+            return Trainings;
+        }
+    }
+}
