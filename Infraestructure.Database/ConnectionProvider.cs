e911e644e1ec5dfa67edaf115364fce307d1c740
CristianQS <cristian_31teror@hotmail.com>
2020-05-02 21:05:49 +0100

    refactor: remove unnecesary code

--- Infraestructure.Database/ConnectionProvider.cs
+++ Infraestructure.Database/ConnectionProvider.cs
@@ -13,25 +13,16 @@ namespace Infraestructure.Database {
             ConnectionString = connectionString;
         }
         public async Task EstablishConnection() {
-            // "Host=localhost;Port=5432;Username=admin;Password=admin;Database=python_db"
-            if (IsOpen()) return;
             Connection = new NpgsqlConnection(ConnectionString);
             await Connection.OpenAsync();
         }
-        public bool IsOpen() {
-            return Connection != null;
-        }
+ 
         public void Close() {
-            if (!IsOpen()) return;
             Connection.Close();
         }
 
         public IEnumerable<T> Query<T>(string sql, object param = null, int? commandTimeout = null) {
             return Connection.Query<T>(sql,param).ToList();
-        }
-
-        public int Execute(string sql, object param = null, int? commandTimeout = null) {
-            return Connection.Execute(sql, param);
         }
 
     }
